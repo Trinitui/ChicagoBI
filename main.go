9cf8b5c3049ac1a192c58089e26863d221c6c8c0
Andrew Whelan <andrewmwhelan9@gmail.com>
2022-08-20 16:15:53 -0400

    debugging

--- main.go
+++ main.go
@@ -126,7 +126,6 @@ func main() {
 		fmt.Println("Done with CovidData")
 		GetBuildingPermits(db)
 		fmt.Println("Done with BP")
-
 		GetUnemploymentData(db)
 		fmt.Println("Done with Unemployment")
 
@@ -543,7 +542,7 @@ func GetTaxiTrips(db *sql.DB) {
 	fmt.Println("JSON unmarshalling done...")
 	fmt.Println("Now unpacking JSON and inserting into db... ")
 	for i := 0; i < len(taxi_trips_list); i++ {
-
+		fmt.Println(i)
 		// We will execute definsive coding to check for messy/dirty/missing data values
 		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
 
